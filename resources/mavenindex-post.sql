e584b22227c8fe651a2034e7c519dc2274b54f17
Luigi <a>
2015-08-28 12:03:51 +0200

    Refactor stats to view into mat tables that show size group by packaging, extension and optional rootgroup.

--- resources/mavenindex-post.sql
+++ resources/mavenindex-post.sql
@@ -3,63 +3,62 @@
 --
 --
 create view artifact_view as
-  select groupid || ':' || artifactid || '@' || version ||  ifnull('/' || classifier, '') || '.' || extension as id, * 
+  select
+    substr(groupid || '.', 1, instr(groupid || '.', '.') - 1) as rootgroup,
+    groupid || ':' || artifactid || '@' || version ||  ifnull('/' || classifier, '') || '.' || extension as id, 
+    * 
   from artifact;
 
 --
 --
 --
 create view artifact_main as
-  select * from artifact where classifier is null;
+  select * from artifact_view where classifier is null;
 
 --
 --
 --
 create view artifact_secondary as
-  select * from artifact where classifier is not null;
+  select * from artifact_view where classifier is not null;
 
 --
 --
 --
 create view artifact_jar as
-  select * from artifact where classifier is null and packaging = 'jar' and extension = 'jar';
-
---
---
---
-create view artifact_war as
-  select * from artifact where classifier is null and packaging = 'war' and extension = 'war';
+  select * from artifact_main where packaging = 'jar' and extension = 'jar';
 
 --
 -- Quick stats of the Maven Index.
 -- Similar to http://search.maven.org/#stats.
 --
-create table stats (
-  gavcp       integer not null, -- Total number of artifacts and secondary artifacts (GAVCP).
-  gav         integer not null, -- Total number of artifacts indexed (GAV).
-  ga          integer not null, -- Total number of unique artifacts indexed (GA).
-  size        integer not null, -- Estimated size of the entire repository (in MB).
-  jarsize     integer not null, -- Estimated size of jar artifacts (in MB).
-  netjarsize  integer not null, -- Estimated size of jar artifacts in the net root group (in MB).
-  comjarsize  integer not null, -- Estimated size of jar artifacts in the net root group (in MB).
-  orgjarsize  integer not null, -- Estimated size of jar artifacts in the net root group (in MB).
-  warsize     integer not null, -- Estimated size of war artifacts (in MB).
-  netwarsize  integer not null, -- Estimated size of war artifacts in the net root group (in MB).
-  comwarsize  integer not null, -- Estimated size of war artifacts in the net root group (in MB).
-  orgwarsize  integer not null  -- Estimated size of war artifacts in the net root group (in MB).
-);
-
-insert into stats (gavcp, gav, ga, size, jarsize, netjarsize, comjarsize, orgjarsize, warsize, netwarsize, comwarsize, orgwarsize)
+create table stats as
   select 
-    (select count(*) from artifact), 
-    (select count(*) from (select distinct groupid, artifactid, version from artifact)), 
-    (select count(*) from (select distinct groupid, artifactid from artifact)), 
-    (select sum(size)/(1024*1024) from artifact), 
-    (select sum(size)/(1024*1024) from artifact_jar), 
-    (select sum(size)/(1024*1024) from artifact_jar where substr(groupid, 1, 4) = 'net.'), 
-    (select sum(size)/(1024*1024) from artifact_jar where substr(groupid, 1, 4) = 'com.'), 
-    (select sum(size)/(1024*1024) from artifact_jar where substr(groupid, 1, 4) = 'org.'), 
-    (select sum(size)/(1024*1024) from artifact_war), 
-    (select sum(size)/(1024*1024) from artifact_war where substr(groupid, 1, 4) = 'net.'), 
-    (select sum(size)/(1024*1024) from artifact_war where substr(groupid, 1, 4) = 'com.'), 
-    (select sum(size)/(1024*1024) from artifact_war where substr(groupid, 1, 4) = 'org.');
+    (select count(*) from artifact)                                                     as gavcp,  -- Total number of artifacts and secondary artifacts (GAVCP).                               
+    (select count(*) from (select distinct groupid, artifactid, version from artifact)) as gav,    -- Total number of artifacts indexed (GAV). 
+    (select count(*) from (select distinct groupid, artifactid from artifact))          as ga,     -- Total number of unique artifacts indexed (GA).
+    (select sum(size)/(1024*1024) from artifact)                                        as size;   -- Estimated size of the entire repository (in MB).             
+
+--
+-- Estimated size of the repository by 
+-- packaging, extension (in MB).
+--    
+create table sizestats_by_packaging_extension as
+  select 
+    packaging, 
+	extension, 
+	sum(size)/(1024*1024) as size 
+  from artifact_main 
+  group by packaging, extension;
+
+--
+-- Estimated size of the repository by 
+-- packaging, extension, rootgroup (in MB).
+--    
+create table sizestats_by_packaging_extension_rootgroup as
+  select 
+    packaging, 
+	extension, 
+	rootgroup, 
+	sum(size)/(1024*1024) as size 
+  from artifact_main 
+  group by packaging, extension, rootgroup;
