1f3402c0e5d42e11adf8c4c906e0ba5d0a5ae0d4
Luigi <a>
2015-09-10 13:49:34 +0200

    Cleaning maven index.

--- sql/mavenindex.sql
+++ sql/mavenindex.sql
@@ -16,27 +16,8 @@ create table artifact (
   packaging     varchar(64)   not null,     -- i[0] == u[4] if not null
   idate         date          not null,     -- ( i[1] )
   size          integer       not null,     -- ( i[2] )
---  is3           integer       not null,     -- ( i[3] )
---  is4           integer       not null,     -- ( i[4] )
---  is5           integer       not null,     -- ( i[5] )
   extension     varchar(64)   not null,     -- Artifact file extension. ( i[6] )
-  mdate         date          not null--,     -- m
---  sha           varchar(128)--,               -- SHA1 hash (optional) ( 1 )
---  artifactname  text,                       -- Artifact title ( n )
---  artifactdesc  text                       -- Artifact description ( d )
-);
-
---
--- Deleted artifacts.
--- These artifacts are not present in the Maven Repository.
---
-create table del ( 
-  groupid       varchar(128)  not null,     -- Group name ( u[0] )
-  artifactid    varchar(128)  not null,     -- Artifact ID ( u[1] )
-  version       varchar(64)   not null,     -- Version ( u[2] )
-  classifier    varchar(64),                -- Secondary artifacts (e.g., 'sources' or 'javadoc'). ( u[3] )
-  packaging     varchar(64),                -- i[0] == u[4] if not null
-  mdate         date          not null     -- Modified date
+  mdate         date          not null      -- m
 );
 
 --
@@ -47,30 +28,6 @@ create table header (
   headb        varchar(32)  not null,
   creationdate date         not null
 );
-
---
--- Descriptor index properties.
--- This table should contain only 1 row.
---
-create table descriptor (
-  DESCRIPTOR   varchar(255) not null,
-  IDXINFO      varchar(255) not null
-);
-
---
--- Contains all maven groups.
---
-create table allgroups (
-  groupid varchar(128) not null primary key
-);
-
---
--- Contains only the root groups.
---
-create table rootgroups (
-  groupid varchar(128) not null primary key
-);
-
 
 
 --
--- src/ch/usi/inf/mavends/PopulateMavenIndex.java
+++ src/ch/usi/inf/mavends/PopulateMavenIndex.java
@@ -2,9 +2,7 @@ package ch.usi.inf.mavends;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 
 import ch.usi.inf.mavends.index.MavenRecord;
 import ch.usi.inf.mavends.index.NexusConstants;
@@ -35,125 +33,48 @@ public class PopulateMavenIndex implements NexusConstants {
 
 	}
 
-	private static ArrayList<byte[]> split(byte[] value) {
-		ArrayList<byte[]> res = new ArrayList<byte[]>();
-
-		int prev = 0;
-		for (int i = 0; i < value.length; i++) {
-			if (value[i] == BAR) {
-				res.add(Arrays.copyOfRange(value, prev, i));
-				prev = i + 1;
-			}
-		}
-
-		res.add(Arrays.copyOfRange(value, prev, value.length));
-
-		return res;
-	}
-
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, IOException,
 			ClassNotFoundException, SQLException {
 		Args ar = ArgsParser.parse(args, new Args());
 
 		try (Db db = new Db(ar.mavenIndex); NexusIndex nip = new NexusIndex(ar.nexusIndex)) {
 			Inserter artins = db
-					.createInserter("insert into artifact (mdate, sha, groupid, artifactid, version, classifier, packaging, idate, size, is3, is4, is5, extension) values (date(?, 'unixepoch' ), ?, ?, ?, ?, ?, ?, date(?, 'unixepoch' ), ?, ?, ?, ?, ?)");
-
-			Inserter delins = db
-					.createInserter("insert into del (groupid, artifactid, version, classifier, packaging, mdate) values (?, ?, ?, ?, ?, date(?, 'unixepoch'))");
-
-			Inserter allins = db.createInserter("insert into allgroups (groupid) values (?)");
-
-			Inserter rootins = db.createInserter("insert into rootgroups (groupid) values (?)");
+					.createInserter("insert into artifact (mdate, groupid, artifactid, version, classifier, packaging, idate, size, extension) values (date(?/1000, 'unixepoch' ), ?, ?, ?, ?, ?, date(?/1000, 'unixepoch' ), ?, ?)");
 
 			Inserter headerins = db
-					.createInserter("insert into header (headb, creationdate) values (?, date(?, 'unixepoch' ))");
-
-			Inserter descins = db.createInserter("insert into descriptor (descriptor, idxinfo) values (?, ?)");
+					.createInserter("insert into header (headb, creationdate) values (?, date(?/1000, 'unixepoch' ))");
 
 			log.info("Inserting header...");
-			headerins.insert(nip.headb, new Date(nip.headl / 1000));
+			headerins.insert(nip.headb, nip.headl);
 
 			while (nip.hasNext()) {
 				NexusRecord nr = nip.next();
 
-				// if (ndocs % 100000 == 0) {
-				// log.info("ndocs: %,d", ndocs);
-				// }
-
-				byte[] m;
 				byte[] u;
-				byte[] i;
-				//byte[] del;
-				//byte[] sha;
-				// byte[] n;
-				// byte[] d;
-				//byte[] descriptor;
 
-				if ((m = nr.get(M)) != null) {
-					if ((u = nr.get(U)) != null) {
-						i = nr.get(I);
-						// sha = nr.get(SHA);
-						// n = nr.get(N);
-						// d = nr.get(D);
+				if ((u = nr.get(U)) != null) {
+					byte[] m = nr.get(M);
+					byte[] i = nr.get(I);
 
-						byte[][] us = MavenRecord.split(u, 5);
+					byte[][] us = MavenRecord.split(u, 5);
 
-						byte[] groupid = us[0];
-						byte[] artifactid = us[1];
-						byte[] version = us[2];
-						byte[] classifier = Arrays.equals(us[3], NA) ? null : us[3];
+					String groupid = new String(us[0]);
+					String artifactid = new String(us[1]);
+					String version = new String(us[2]);
+					String classifier = Arrays.equals(us[3], NA) ? null : new String(us[3]);
 
-						byte[][] is = MavenRecord.split(i, 7);
+					byte[][] is = MavenRecord.split(i, 7);
 
-						byte[] packaging = is[0];
-						byte[] idate = is[1];
-						byte[] size = is[2];
-						// byte[] is3 = is[3];
-						// byte[] is4 = is[4];
-						// byte[] is5 = is[5];
-						byte[] extension = is[6];
+					String packaging = new String(is[0]);
+					byte[] idate = is[1];
+					byte[] size = is[2];
+					String extension = new String(is[6]);
 
-						artins.insert(m, groupid, artifactid, version, classifier, packaging, idate, size, extension);
-					} 
-//					else if ((del = nr.get(DEL)) != null) {
-//						byte[][] dels = MavenRecord.split(del, 5);
-//
-//						byte[] groupid = dels[0];
-//						byte[] artifactid = dels[1];
-//						byte[] version = dels[2];
-//						byte[] classifier = Arrays.equals(dels[3], NA) ? null : dels[3];
-//						byte[] packaging = dels[3] != null ? null : dels[4];
-//
-//						// delins.insert(groupid, artifactid, version,
-//						// classifier, packaging, m);
-//					}
-//				} else if ((descriptor = nr.get(DESCRIPTOR)) != null) {
-//					log.info("Inserting descriptor...");
-//
-//					byte[] idxinfo = nr.get(IDXINFO);
-//
-//					// descins.insert(descriptor, idxinfo);
-//				} else if (nr.get(ALL_GROUPS) != null) {
-//					log.info("Inserting allGroups...");
-//
-//					byte[] allGroupsList = nr.get(ALL_GROUPS_LIST);
-//
-//					for (byte[] groupid : split(allGroupsList)) {
-//						// allins.insert(groupid);
-//					}
-//				} else if (nr.get(ROOT_GROUPS) != null) {
-//					log.info("Inserting rootGroups...");
-//
-//					byte[] rootGroupsList = nr.get(ROOT_GROUPS_LIST);
-//
-//					for (byte[] groupid : split(rootGroupsList)) {
-//						// rootins.insert(groupid);
-//					}
+					artins.insert(m, groupid, artifactid, version, classifier, packaging, idate, size, extension);
 				}
 			}
 
-			log.info("ndocs: %,d", nip.nrecs);
+			log.info("Number of Records: %,d", nip.nrecs);
 
 			db.conn.commit();
 		}
