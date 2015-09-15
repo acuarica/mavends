ae1b6ef021dbb4621f2e90b908d657a343aa0e38
Luigi <a>
2015-09-15 16:36:28 +0200

    Before delete method compress.

--- src/ch/usi/inf/mavends/inode/Helper.java
+++ src/ch/usi/inf/mavends/inode/Helper.java
@@ -24,18 +24,18 @@ final class Helper {
 		}
 	}
 
-	static byte[] compress(byte[] data) throws IOException {
-		Deflater deflater = new Deflater();
-		deflater.setInput(data);
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-		deflater.finish();
-		byte[] buffer = new byte[1024];
-		while (!deflater.finished()) {
-			int count = deflater.deflate(buffer);
-			outputStream.write(buffer, 0, count);
-		}
-		outputStream.close();
-		byte[] output = outputStream.toByteArray();
-		return output;
-	}
+//	static byte[] compress(byte[] data) throws IOException {
+//		Deflater deflater = new Deflater();
+//		deflater.setInput(data);
+//		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+//		deflater.finish();
+//		byte[] buffer = new byte[1024];
+//		while (!deflater.finished()) {
+//			int count = deflater.deflate(buffer);
+//			outputStream.write(buffer, 0, count);
+//		}
+//		outputStream.close();
+//		byte[] output = outputStream.toByteArray();
+//		return output;
+//	}
 }
