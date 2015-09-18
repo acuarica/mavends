2960600057820bb73997c4788179077af8b1dbaa
Luigi <a>
2015-09-18 17:27:08 +0200

    Progress

--- README.md
+++ README.md
@@ -119,3 +119,66 @@ Refer to [sql/mavenindex.sql] for more details on the tables and views of the Ma
     ant <conf> analysis-unsafe
 
 ASM
+
+
+```java
+public class StatsVisitor extends MavenVisitor {
+
+	private static final Log log = new Log(System.out);
+
+	private long classCount = 0;
+	private long methodCount = 0;
+	private long callsiteCount = 0;
+	private long fielduseCount = 0;
+	private long constantCount = 0;
+
+	@Override
+	public ClassVisitor visitClass() {
+		return new ClassVisitor(Opcodes.ASM5) {
+
+			@Override
+			public void visit(int version, int access, String name, String signature, String superName,
+					String[] interfaces) {
+				super.visit(version, access, name, signature, superName, interfaces);
+				classCount++;
+			}
+
+			@Override
+			public MethodVisitor visitMethod(int access, final String methodName, final String methodDesc,
+					String signature, String[] exceptions) {
+
+				methodCount++;
+
+				MethodVisitor mv = new MethodVisitor(Opcodes.ASM5) {
+
+					@Override
+					public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+						callsiteCount++;
+					}
+
+					@Override
+					public void visitFieldInsn(int opcode, String owner, String name, String desc) {
+						fielduseCount++;
+					};
+
+					@Override
+					public void visitLdcInsn(Object cst) {
+						constantCount++;
+					}
+				};
+
+				return mv;
+			}
+		};
+	}
+
+	@Override
+	public void close() {
+		log.info("Number of classes: %,d", classCount);
+		log.info("Number of methods: %,d", methodCount);
+		log.info("Number of call sites: %,d", callsiteCount);
+		log.info("Number of field uses: %,d", fielduseCount);
+		log.info("Number of constants: %,d", constantCount);
+	}
+}
+```
--- src/ch/usi/inf/mavends/analysis/visitors/StatsVisitor.java
+++ src/ch/usi/inf/mavends/analysis/visitors/StatsVisitor.java
@@ -7,15 +7,20 @@ import org.objectweb.asm.Opcodes;
 import ch.usi.inf.mavends.analysis.MavenVisitor;
 import ch.usi.inf.mavends.util.log.Log;
 
+/**
+ * 
+ * @author Luis Mastrangelo (luis.mastrangelo@usi.ch)
+ *
+ */
 public class StatsVisitor extends MavenVisitor {
 
 	private static final Log log = new Log(System.out);
 
-	private long noclasses = 0;
-	private long nomethods = 0;
-	private long nocallsites = 0;
-	private long nofielduses = 0;
-	private long noliteral = 0;
+	private long classCount = 0;
+	private long methodCount = 0;
+	private long callsiteCount = 0;
+	private long fielduseCount = 0;
+	private long constantCount = 0;
 
 	@Override
 	public ClassVisitor visitClass() {
@@ -25,30 +30,30 @@ public class StatsVisitor extends MavenVisitor {
 			public void visit(int version, int access, String name, String signature, String superName,
 					String[] interfaces) {
 				super.visit(version, access, name, signature, superName, interfaces);
-				noclasses++;
+				classCount++;
 			}
 
 			@Override
 			public MethodVisitor visitMethod(int access, final String methodName, final String methodDesc,
 					String signature, String[] exceptions) {
 
-				nomethods++;
+				methodCount++;
 
 				MethodVisitor mv = new MethodVisitor(Opcodes.ASM5) {
 
 					@Override
 					public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
-						nocallsites++;
+						callsiteCount++;
 					}
 
 					@Override
 					public void visitFieldInsn(int opcode, String owner, String name, String desc) {
-						nofielduses++;
+						fielduseCount++;
 					};
 
 					@Override
 					public void visitLdcInsn(Object cst) {
-						noliteral++;
+						constantCount++;
 					}
 				};
 
@@ -59,10 +64,10 @@ public class StatsVisitor extends MavenVisitor {
 
 	@Override
 	public void close() {
-		log.info("No classes: %,d", noclasses);
-		log.info("No methods: %,d", nomethods);
-		log.info("No callsites: %,d", nocallsites);
-		log.info("No field uses: %,d", nofielduses);
-		log.info("No literal: %,d", noliteral);
+		log.info("Number of classes: %,d", classCount);
+		log.info("Number of methods: %,d", methodCount);
+		log.info("Number of call sites: %,d", callsiteCount);
+		log.info("Number of field uses: %,d", fielduseCount);
+		log.info("Number of constants: %,d", constantCount);
 	}
 }
