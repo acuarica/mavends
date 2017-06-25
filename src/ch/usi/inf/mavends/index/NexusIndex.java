cebdc68860ed88b3104bf459f000e7153c0a99ec
luigi <luismastrangelo@usi.ch>
2017-06-25 16:26:39 +0200

    support for nexus index bigger that 2gb

--- src/ch/usi/inf/mavends/index/NexusIndex.java
+++ src/ch/usi/inf/mavends/index/NexusIndex.java
@@ -1,10 +1,8 @@
 package ch.usi.inf.mavends.index;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 import java.util.Date;
 
 /**
@@ -13,18 +11,13 @@ import java.util.Date;
  * @author Luis Mastrangelo
  *
  */
-final class NexusIndex implements AutoCloseable {
-
-//    private final int BLOCK_SIZE = Integer.MAX_VALUE;
-    private final int BLOCK_SIZE = 10000*1000;
-
-	private final RandomAccessFile raf;
-	private final FileChannel fc;
-	private MappedByteBuffer mbb = null;
+final class NexusIndex {
 
 	final int headb;
 
 	final Date creationDate;
+
+	private final DataInputStream dis;
 
 	/**
 	 * Creates a new parser with the specified path. The indexPath must be a
@@ -35,108 +28,33 @@ final class NexusIndex implements AutoCloseable {
 	 * @throws IOException
 	 */
 	NexusIndex(String indexPath) throws IOException {
-		raf = new RandomAccessFile(indexPath, "r");
-		fc = raf.getChannel();
-
-		System.out.println(Integer.MAX_VALUE);
-		map(0);
-
-		headb = mbb.get();
-		creationDate = new Date(mbb.getLong());
+        dis = new DataInputStream(new FileInputStream(indexPath));
+		headb = dis.readByte();
+		creationDate = new Date(dis.readLong());
 	}
 
-	private void map(int position) throws IOException {
-	    System.out.println("position: " + position);
-	    if (fc.size() - position > BLOCK_SIZE) {
-           mbb = fc.map(FileChannel.MapMode.READ_ONLY, position, BLOCK_SIZE);
-       } else {
-           mbb = fc.map(FileChannel.MapMode.READ_ONLY, position, fc.size() - position);
-       }
-    }
-
-    private int n = 10000;
-
-	boolean hasNext() {
-	    n--;
-
-	    if (n == 0) {
-	        return false;
-        }
-
-		return mbb.hasRemaining();
+	boolean hasNext() throws IOException {
+	    return dis.available() > 0;
 	}
 
 	NexusRecord next() throws IOException {
-		final int fieldCount = getInt();
+		final int fieldCount = dis.readInt();
 		final NexusRecord nr = new NexusRecord(fieldCount);
 
 		for (int i = 0; i < fieldCount; i++) {
-			get();
+			dis.readByte();
 
-			int keyLen = getShort();
+			int keyLen = dis.readShort();
 			byte[] key = new byte[keyLen];
-			get(key);
+			dis.read(key);
 
-			int valueLen = getInt();
+			int valueLen = dis.readInt();
 			byte[] value = new byte[valueLen];
-			get(value);
+			dis.read(value);
 
 			nr.put(i, key, value);
 		}
 
 		return nr;
-	}
-
-	private int getInt() throws IOException {
-	   if (mbb.position() + 4 > BLOCK_SIZE) {
-           byte[] value = new byte[4];
-           get(value);
-           return ByteBuffer.wrap(value).getInt();
-       }
-
-       return mbb.getInt();
-    }
-
-    private int getShort() throws IOException {
-        if (mbb.position() + 2 > BLOCK_SIZE) {
-            byte[] value = new byte[2];
-            get(value);
-            return ByteBuffer.wrap(value).getShort();
-        }
-
-        return mbb.getShort();
-    }
-
-    private byte get() throws IOException {
-        if (mbb.position() + 1 > BLOCK_SIZE) {
-            byte[] value = new byte[1];
-            get(value);
-            return ByteBuffer.wrap(value).get();
-        }
-
-        return mbb.get();
-    }
-
-    private void get(byte[] value) throws IOException {
-        if (mbb.position() + value.length > BLOCK_SIZE) {
-            System.out.print(mbb.position());
-            System.out.print(", ");
-            System.out.print(value.length);
-            System.out.print(", ");
-            System.out.println(BLOCK_SIZE);
-
-            int r = BLOCK_SIZE - mbb.position();
-            mbb.get(value, 0, r);
-            map(mbb.position());
-            mbb.get(value, r, value.length - r);
-        }
-
-        mbb.get(value);
-    }
-
-	@Override
-	public void close() throws IOException {
-		fc.close();
-		raf.close();
 	}
 }
