1c6557d1025d3ee1bc5e74b95840dfb1542fc3d2
luigi <luismastrangelo@usi.ch>
2017-07-21 12:55:08 +0200

    casts

--- src/ch/usi/inf/mavends/analysis/visitors/CastStatsVisitor.java
+++ src/ch/usi/inf/mavends/analysis/visitors/CastStatsVisitor.java
@@ -3,13 +3,19 @@ package ch.usi.inf.mavends.analysis.visitors;
 import ch.usi.inf.mavends.util.extract.Artifact;
 import ch.usi.inf.mavends.util.extract.MavenVisitor;
 import org.objectweb.asm.*;
+import org.objectweb.asm.util.Printer;
+import org.objectweb.asm.util.Textifier;
+import org.objectweb.asm.util.TraceMethodVisitor;
 
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class CastStatsVisitor extends MavenVisitor {
 
 	private final PrintStream out;
+
+    private boolean hasCheckcast;
 
 	public CastStatsVisitor() throws FileNotFoundException {
 		out = new PrintStream("out/casts.csv");
@@ -32,32 +38,42 @@ public class CastStatsVisitor extends MavenVisitor {
 			public MethodVisitor visitMethod(int access, final String methodName, final String methodDesc,
 					String signature, String[] exceptions) {
 
+                hasCheckcast = false;
+            Printer p = new Textifier(Opcodes.ASM5) {
+                            @Override
+                            public void visitMethodEnd() {
+                                if (hasCheckcast) {
+                                    String m = String.format("---- Method %s.%s ----", className, methodName);
+                                    System.out.println(m);
+                                    print(new PrintWriter(System.out));
+                                }
+                            }
+                        };
+
 				MethodVisitor mv = new MethodVisitor(Opcodes.ASM5) {
 
-				    @Override
+                    @Override
 					public void visitTypeInsn(int opcode, String type) {
                         if (opcode == Opcodes.CHECKCAST) {
-                            add("CHECKCAST", type, "");
+                            hasCheckcast = true;
                         } else if (opcode == Opcodes.INSTANCEOF) {
-                            add("INSTANCEOF", type, "");
 						}
 					}
 
 					public void visitTryCatchBlock(Label start, Label end, Label handler, String type) {
 				        if ("java/lang/ClassCastException".equals(type)) {
-                            add("ClassCastException", type, "");
+//                            add("ClassCastException", type, "");
                         }
 					}
 
 					private void add(String owner, String name, String desc) {
-						out.format("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", art.coordid, art.groupid, art.artifactid,
-								art.version, art.idate, art.mdate, className, methodName, methodDesc, owner, name, desc);
+//						out.format("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", art.coordid, art.groupid, art.artifactid,
+//								art.version, art.idate, art.mdate, className, methodName, methodDesc, owner, name, desc);
 					}
 				};
 
-				return mv;
+				return new TraceMethodVisitor(mv, p);
 			}
-
 		};
 	}
 
